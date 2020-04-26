5bba0909bad52643d2b6c511409821efb21c03a9
Corinna Bürger <corinna_buerger@yahoo.de>
2020-04-26 23:13:38 +0200

    add first part of new colorbar code

--- tpaint.py
+++ tpaint.py
@@ -1,6 +1,5 @@
-import turtle
 from sys import exit
-from turtle import Screen, Turtle
+from turtle import Screen, Turtle, mainloop
 
 from libturtle import StringToggler, VolumeBar
 
@@ -18,6 +17,7 @@ class TurtlePaint():
     def __init__(self, settings):
         self.t1 = Turtle()  # main turtle
         self.t2 = Turtle()  # draws color bar
+        self.t3 = Turtle()  # selects a color from the color bar
         self.s = Screen()
         self.v = VolumeBar(settings.get("start_volume_bar"),
                            settings.get("max_pensize"), self.s)
@@ -66,60 +66,60 @@ class TurtlePaint():
             self.toggler.toggle()
 
     def draw_color(self):
-        s.tracer(False)
+        self.s.tracer(False)
         x = -370
         pos = (x, 310)
-        t2.penup()
-        t3.penup()
-        t2.goto(pos)
-        t3.goto(pos)
-        t2.right(90)
-        t3.right(90)
-        t2.pendown()
-        t3.pendown()
-        t2.pensize(1)
-        t3.pensize(5)
+        self.t2.penup()
+        self.t3.penup()
+        self.t2.goto(pos)
+        self.t3.goto(pos)
+        self.t2.right(90)
+        self.t3.right(90)
+        self.t2.pendown()
+        self.t3.pendown()
+        self.t2.pensize(1)
+        self.t3.pensize(5)
         for _ in range(4):
-            t3.fd(30)
-            t3.left(90)
-        for color in colors:
+            self.t3.fd(30)
+            self.t3.left(90)
+        for color in self.colors:
             # determine colors
             if color == "white":
-                t2.pencolor("black")
-                t2.fillcolor("white")
+                self.t2.pencolor("black")
+                self.t2.fillcolor("white")
             else:
-                t2.color(color)
+                self.t2.color(color)
 
             # draw square
-            t2.begin_fill()
+            self.t2.begin_fill()
             for _ in range(4):
-                t2.fd(30)
-                t2.left(90)
+                self.t2.fd(30)
+                self.t2.left(90)
 
             # pen up, go to next square, pen down
-            t2.end_fill()
-            t2.penup()
+            self.t2.end_fill()
+            self.t2.penup()
             x += 40
-            t2.goto(x, 310)
-            t2.pendown()
-        s.tracer(True)
+            self.t2.goto(x, 310)
+            self.t2.pendown()
+        self.s.tracer(True)
 
     def return_pos_click(self):
         self.s.onscreenclick(self.t2.goto)
         return self.t2.pos()
 
     def change_color(self):
-        color = colors.pop(0)
-        t1.pencolor(color)
-        colors.append(color)
-        t3.pensize(3)
-        s.tracer(False)
+        color = self.colors.pop(0)
+        self.t1.pencolor(color)
+        self.colors.append(color)
+        self.t3.pensize(3)
+        self.s.tracer(False)
         if color == "black":
-            t3.pencolor("dim gray")
+            self.t3.pencolor("dim gray")
         else:
-            t3.pencolor("black")
-        t3.penup()
-        t3.goto
+            self.t3.pencolor("black")
+        self.t3.penup()
+        self.t3.goto
 
         s.tracer(True)
 
@@ -146,7 +146,7 @@ class TurtlePaint():
         self.s.onkey(self.decrease_pensize, "Down")
 
     def mainloop(self):
-        turtle.mainloop()
+        mainloop()
 
 
 if __name__ == "__main__":
