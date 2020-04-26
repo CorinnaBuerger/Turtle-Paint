8dbe9f848aefee52b0bb2dcfb7cd59cb683a0a91
Corinna Bürger <corinna_buerger@yahoo.de>
2020-04-26 22:23:25 +0200

    refactor classes, implement nicer mainloop

---
+++ libturtle.py
@@ -0,0 +1,84 @@
+from turtle import Screen, Turtle
+
+class VolumeBar():
+    def __init__(self, pos, maximum, screen):
+        self.bar = Turtle()
+        self.fill = Turtle()
+        self.bar_hypo = 100
+        self.pencolor = "black"
+        self.pensize = 1
+        self.pos = pos 
+        self.maximum = maximum
+        self.s = screen
+
+    def draw_volume_bar(self):
+        self.bar.hideturtle()
+        self.s.tracer(False)
+        self.bar.pencolor(self.pencolor)
+        self.bar.pensize(self.pensize)
+        self.bar.penup()
+        self.bar.goto(self.pos)
+        self.bar.pendown()
+        self.bar.left(10)
+        self.bar.fd(self.bar_hypo)
+        self.bar.right(100)
+        self.bar.fd(self.maximum)
+        self.bar.right(90)
+        self.bar.goto(self.pos)
+        self.s.tracer(True)
+
+    def fill_volume_bar(self, percent=None):
+        self.fill.hideturtle()
+        self.fill.clear()
+        self.s.tracer(False)
+        forward = self.bar_hypo * percent 
+        down = self.maximum * percent 
+        self.fill.pencolor(self.pencolor)
+        self.fill.fillcolor(self.pencolor)
+        self.fill.penup()
+        self.fill.goto(self.pos)
+        self.fill.pendown()
+        self.fill.begin_fill()
+        self.fill.left(10)
+        self.fill.fd(forward)
+        self.fill.right(100)
+        self.fill.fd(down)
+        self.fill.right(90)
+        self.fill.goto(self.pos)
+        self.fill.right(180)
+        self.fill.end_fill()
+        self.s.tracer(True)
+
+class StringToggler():
+    def __init__(self, pos_first, pos_second, str_1, str_2, screen):
+        self.write_turtle = Turtle()
+        self.non_active_color = "light gray"
+        self.active_color = "black"
+        self.active_str = (str_1, pos_first)
+        self.passive_str = (str_2, pos_second)
+        self.font = 'Courier', 10, 'italic'
+        self.write_turtle.hideturtle()
+        self.s = screen
+        self.write()
+
+    def write(self):
+        # write strings
+        self.s.tracer(False)
+        self.write_turtle.clear()
+        self.write_turtle.penup()
+        self.write_turtle.goto(self.active_str[1])
+        self.write_turtle.pendown()
+
+        self.write_turtle.color(self.active_color)
+        self.write_turtle.write(self.active_str[0], font = self.font)
+        self.write_turtle.penup()
+        self.write_turtle.goto(self.passive_str[1])
+        self.write_turtle.pendown()
+        self.write_turtle.color(self.non_active_color)
+        self.write_turtle.write(self.passive_str[0], font = self.font)
+        self.s.tracer(True)
+
+    def toggle(self):
+        # toggle strings
+        self.active_str, self.passive_str = self.passive_str, self.active_str
+        self.write()
--- onclick.py
+++ onclick.py
@@ -1,56 +1,69 @@
 from turtle import Screen, Turtle
+from sys import exit
+from libturtle import VolumeBar, StringToggler
 import time
 
-t1 = Turtle()
-t2 = Turtle()
-t3 = Turtle()
-s1 = Screen()
-s1.screensize(600, 600)
-t1.resizemode("auto")
-
-MAX_PENSIZE = 18
+START_VOLUME_BAR = (-370, 250)
+START_PENUP = (0, 300)
+START_PENDOWN = (0, 280)
+MAX_PENSIZE = 21
 PENSIZE_STEPSIZE = 2
-colors = ["green", "yellow", "pink", "blue", "lightblue", "orange", "purple", "black", "white"]
-    
+
 def register_callbacks():
     t1.onclick(t1.goto, 1)
-    s1.onscreenclick(t1.goto, 1)
+    s.onscreenclick(t1.goto, 1)
     t1.onclick(pen_change, 3)
+    s.onkey(screen_exit_handler, "Escape")
+
+def screen_exit_handler():
+    print("[debug] got ESC, quitting")
+    exit(0)
 
 def pen_change(xdummy, ydummy):
+    global toggler
+
     if t1.isdown():
-        # write new state
+        # TODO(coco): update current drawing state text box
         t1.penup()
+        toggler.toggle()
     else:
-        # write new state
+        # TODO(coco): update current drawing state text box
         t1.pendown()
+        toggler.toggle()
 
 def draw_color():
-    t3.speed(30)
+    s.tracer(False)
     x = -370
     pos = (x, 310)
     t3.penup()
     t3.goto(pos)
     t3.right(90)
     t3.pendown()
+    t3.pensize(2)
     for color in colors:
+        # determine colors
         if color == "white":
             t3.pencolor("black")
+            t3.fillcolor("white")
         else:
-            t3.pencolor(color)
-        t3.fillcolor(color)
+            t3.color(color)
+
+        # draw square
         t3.begin_fill()
         for _ in range(4):
             t3.fd(30)
             t3.left(90)
+
+        # pen up, go to next square, pen down
         t3.end_fill()
         t3.penup()
         x += 40
         t3.goto(x, 310)
+        t3.pendown()
+    s.tracer(True)
 
 def return_pos_click():
-	s1.onscreenclick(t3.goto)
-	print(t3.pos())
+	s.onscreenclick(t3.goto)
 	return t3.pos()
 
 def change_color():
@@ -59,38 +72,49 @@ def change_color():
     colors.append(color)
 
 def set_color():
-    s1.onkey(change_color, "space")
-    s1.listen()
+    s.onkey(change_color, "space")
+    s.listen()
 
 def increase_pensize():
     pensize = t1.pensize() + PENSIZE_STEPSIZE
-    if pensize < MAX_PENSIZE:
+    if pensize <= MAX_PENSIZE:
         t1.pensize(pensize)
+        v.fill_volume_bar(t1.pensize/MAX_PENSIZE)
 
 def decrease_pensize():
     pensize = t1.pensize() - PENSIZE_STEPSIZE
     if pensize >= 1:
         t1.pensize(pensize)
+        v.fill_volume_bar(t1.pensize/MAX_PENSIZE)
 
 def set_pensize():
-    s1.onkey(increase_pensize, "Up")
-    s1.onkey(decrease_pensize, "Down")
+    s.onkey(increase_pensize, "Up")
+    s.onkey(decrease_pensize, "Down")
     
-
-if __name__ == "__main__":
-    s1.title("Paint, v0.0.1")
-    draw_color()
-    return_pos_click()
-    set_color()
-    set_pensize()
+def setup():
     t2.penup()
     t2.hideturtle()
     t3.hideturtle()
     t1.shape("turtle")
-    t1.resizemode("user")  # TODO(coco): what is this, exactly?
-    register_callbacks()
-    x = 1
-    y = x
+    t1.resizemode("auto")  # turtlesize increases with pensize
+    s.screensize(600, 600)
+    s.title("Paint, v0.0.1")
 
-    while True:
-        t2.forward(1)
+if __name__ == "__main__":
+    t1 = Turtle() # main turtle
+    t2 = Turtle() # runs hidden so that the program doesn't crash
+    t3 = Turtle() # draws color bar
+    s = Screen()
+    v = VolumeBar(START_VOLUME_BAR, MAX_PENSIZE, s)
+    toggler = StringToggler(START_PENUP, START_PENDOWN, "pen down", "pen up", s)
+    colors = ["green", "yellow", "pink", "blue", "lightblue", "orange", "purple", "black", "white"]
+
+    setup()
+    draw_color()
+    v.draw_volume_bar()
+    v.fill_volume_bar(t1.pensize()/MAX_PENSIZE)
+    return_pos_click()
+    set_color()
+    set_pensize()
+    register_callbacks()
+    s.mainloop()
